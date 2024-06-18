5c8c08f364850951637a2a2981cce03d1a317f83
johnkatua <johnkatua99@gmail.com>
2024-06-18 09:32:33 +0300

    contain duplication solution

---
+++ Warmup/containsDuplicate.js
@@ -0,0 +1,35 @@
+/**
+ * 217: Contains Duplicate
+ * 
+ * Problem Statement:
+ * Given an integer array nums, return true if any value appears at 
+ * least twice in the array, and return false if every element is distinct.
+ * 
+ * Example 1:
+ * Input: nums = [1, 2, 3, 1]
+ * Output: true
+ * 
+ * Example 2:
+ * Input: nums = [1, 2, 3, 4]
+ * Output: false
+ * 
+ * Approach:
+ * Remove all the duplicates from the existing array if they are any.
+ * Compare the length of the given arr with the length of the new array with no duplicates.
+ * If the length do not match then the original array had duplicates,
+ * else the original had no duplicates
+ */
+
+
+const containsDuplicate = nums => {
+  const newArr = [...new Set(nums)]
+  return nums.length !== newArr.length
+}
+
+let input1 = [1,2,3,4]
+let output1 = containsDuplicate(input1)
+console.log(output1); // false
+
+let input2 = [1,2,3,1]
+let output2 = containsDuplicate(input2)
+console.log(output2); // true
